5567206172e2a6e4b619c6138820ea036014bb37
s-waite <samwaite11@gmail.com>
2023-03-17 19:37:21 -0400

    change order of colorscheme call

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("user.plugins")
-require("user.colorscheme")
 require("user.options")
+require("user.colorscheme")
 --[[ require("user.keymaps") ]]
 --[[ require("user.cmp") ]]
 --[[ require("user.lsp") ]]
